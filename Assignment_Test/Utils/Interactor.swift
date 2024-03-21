b49df361f862a6909eb528a237cd5df92c885583
Rakesh <rakeshkumarjogi@gmail.com>
2024-03-20 20:33:54 -0400

    comment

--- Assignment_Test/Utils/Interactor.swift
+++ Assignment_Test/Utils/Interactor.swift
@@ -1,10 +1,14 @@
+// Interactor.swift
 //
-//  Interactor.swift
-//
+// This file contains the implementation of the Interactor class, which is used to manage custom interactive transitions for UIViewController animations.
 
 import UIKit
 
+/// The Interactor class conforms to the UIPercentDrivenInteractiveTransition protocol, which allows it to manage interactive transitions for UIViewController animations.
 class Interactor: UIPercentDrivenInteractiveTransition {
+    /// A boolean value that indicates whether the interactive transition has started.
     var hasStarted = false
+    
+    /// A boolean value that indicates whether the interactive transition should finish.
     var shouldFinish = false
 }
