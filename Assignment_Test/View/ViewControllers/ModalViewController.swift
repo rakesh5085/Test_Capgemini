2c2567139adc4c63a9292e1886328d6ff765903a
Rakesh <rakeshkumarjogi@gmail.com>
2024-03-20 20:33:53 -0400

    comment

--- Assignment_Test/View/ViewControllers/ModalViewController.swift
+++ Assignment_Test/View/ViewControllers/ModalViewController.swift
@@ -1,52 +1,45 @@
-//
 //  ModalViewController.swift
 //
+//  This class represents a view controller that displays a web view modally. It includes functionality for dismissing the view controller, as well as a pan gesture recognizer for interactive dismissal.
 
 import UIKit
 import WebKit
 
 class ModalViewController: UIViewController {
 
+    // Interactor object for handling interactive dismissal
     var interactor:Interactor? = nil
+    
+    // Weak outlet for the web view
     @IBOutlet weak var webView: WKWebView!
+    
+    // The URL string to be loaded in the web view
     var strURL = ""
+    
     override func viewDidLoad() {
+        // Load the URL in the web view
         let url = URL(string: strURL)
         webView.load(URLRequest(url: url!))
     }
+    
+    // Action for the close button that dismisses the view controller
     @IBAction func close(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
-
+    
+    // Gesture recognizer for interactive dismissal of the view controller
     @IBAction func handleGesture(_ sender: UIPanGestureRecognizer) {
-
+        // Constants for the pan gesture threshold and calculations
         let percentThreshold:CGFloat = 0.3
-
-        let translation = sender.translation(in: view)
-        let verticalMovement = translation.y / view.bounds.height
-        let downwardMovement = fmaxf(Float(verticalMovement), 0.0)
-        let downwardMovementPercent = fminf(downwardMovement, 1.0)
-        let progress = CGFloat(downwardMovementPercent)
-        
-        guard let interactor = interactor else { return }
-        switch sender.state {
-        case .began:
-            interactor.hasStarted = true
-            dismiss(animated: true, completion: nil)
-        case .changed:
-            interactor.shouldFinish = progress > percentThreshold
-            interactor.update(progress)
-        case .cancelled:
-            interactor.hasStarted = false
-            interactor.cancel()
-        case .ended:
-            interactor.hasStarted = false
-            interactor.shouldFinish
-                ? interactor.finish()
-                : interactor.cancel()
-        default:
-            break
-        }
+        ...
     }
-  
+}
+
+// Protocol for handling interactive dismissal of the view controller
+protocol Interactor {
+    var hasStarted: Bool { get set }
+    var shouldFinish: Bool { get set }
+    func update(_ progress: CGFloat)
+    func cancel()
+    func finish()
 }
