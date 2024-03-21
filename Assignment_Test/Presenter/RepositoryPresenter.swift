d595404bdfa96efb6b4667b5c7df3f5647c7a585
Rakesh <rakeshkumarjogi@gmail.com>
2024-03-20 20:33:49 -0400

    comment

--- Assignment_Test/Presenter/RepositoryPresenter.swift
+++ Assignment_Test/Presenter/RepositoryPresenter.swift
@@ -1,56 +1,58 @@
-//
 //  RepoPresenter.swift
 //
 
 import Foundation
 
+// Define a protocol for the RepositoryView, which outlines the required methods for the view to implement
 protocol RepositoryView: NSObjectProtocol {
-    func setRepositoryList(repositoryList: [Repository])
-    func setEmptyRepositoryList()
-    func updateView(displayName: String)
+    func setRepositoryList(repositoryList: [Repository]) // Set the list of repositories
+    func setEmptyRepositoryList() // Set the list of repositories to empty
+    func updateView(displayName: String) // Update the view with the user's display name
 }
 
+// RepositoryPresenter class, which acts as the intermediary between the GithubService and the RepositoryView
 class RepositoryPresenter {
-    private let githubService: GithubService
-    weak private var repositoryView: RepositoryView?
+    private let githubService: GithubService // A reference to the GithubService instance
+    weak private var repositoryView: RepositoryView? // A weak reference to the RepositoryView instance
     
     init(githubService: GithubService) {
         self.githubService = githubService
     }
     
+    // Attach the view to the presenter
     func attachView(view: RepositoryView) {
         repositoryView = view
     }
     
+    // Detach the view from the presenter
     func detachView() {
         repositoryView = nil
     }
     
+    // Fetch the repository list based on the search text
     func getRepositoryList(searchText : String) {
-        let repoUrl =  Constants.URL.searchUrl + "" + searchText
-        githubService.getSearchRepo(url: repoUrl,
-            onSuccess: { [weak self] repoList in
+        let repoUrl =  Constants.URL.searchUrl + "" + searchText // Construct the search URL
+        githubService.getSearchRepo(url: repoUrl, // Call the GithubService's getSearchRepo method
+            onSuccess: { [weak self] repoList in // On success
                 guard let ws = self else { return }
-                if (repoList.count == 0){
-                    ws.repositoryView?.setEmptyRepositoryList()
+                if (repoList.count == 0){ // If the list is empty
+                    ws.repositoryView?.setEmptyRepositoryList() // Notify the view to set the empty list
                 } else {
-                    var respositoryList = [Repository]()
-                    for item in repoList {
-                        let single = Repository.build(item)
-                        respositoryList.append(single)
+                    var respositoryList = [Repository]() // Initialize an empty list
+                    for item in repoList { // Iterate through the received list
+                        let single = Repository.build(item) // Create a Repository instance from the item
+                        respositoryList.append(single) // Add the Repository instance to the list
                     }
-                    ws.repositoryView?.setRepositoryList(repositoryList: respositoryList)
+                    ws.repositoryView?.setRepositoryList(repositoryList: respositoryList) // Notify the view to set the list
                 }
             },
-            onFailure: { [weak self] errorMessage in
-                self?.repositoryView?.setEmptyRepositoryList()
+            onFailure: { [weak self] errorMessage in // On failure
+                self?.repositoryView?.setEmptyRepositoryList() // Notify the view to set the empty list
             }
         )
     }
     
+    // Fetch the user's profile based on the auth code
     func getUserProfile(authCode: String){
-        githubService.getUserProfile(authCode: authCode) { displayName in
-            self.repositoryView?.updateView(displayName: displayName)
-        } onFailure: { errorMessage in }
-    }
-}
+        githubService.getUserProfile(authCode: authCode) { displayName in // Call the GithubService's getUserProfile method
+            self.repositoryView?.updateView(displayName: displayName) // Notify the view to update with the
